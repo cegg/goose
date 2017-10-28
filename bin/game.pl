f3bb2cb6c2f1e1981e99be327156b640a81fb221
Igor Pozdnyakov <igor.pozdnyakov@nanoporetech.com>
2017-10-28 16:02:23 +0100

    user input parsing, started on the package

--- bin/game.pl
+++ bin/game.pl
@@ -3,85 +3,202 @@
 use strict;
 use warnings;
 use DateTime;
-use JSON;
 use Getopt::Long;
 use English qw(-no_match_vars);
 use Carp;
-use lib qw(lib /opt/goose/lib);
+#use lib qw(../lib /opt/goose/lib); # if I get to to the point of making proper deb
 use Data::Dumper;
 use Readonly;
-use Cwd;
-use Config::IniFiles;
+use Cwd qw(getcwd);
+use File::Path qw(make_path);
+
+use FindBin; # might or might not be installed
+use File::Spec;
+use lib File::Spec->catdir($FindBin::Bin, '..', 'lib');
+use player;
 
 our $VERSION = q[0.0.1];
 
-main();
+$SIG{INT} = \&signal_handler;
+$SIG{TERM} = \&signal_handler;
+
+
+our  %OPTIONS = (
+  help  => qq[$PROGRAM_NAME v$VERSION
+
+     The Game of the Goose.
+
+     --help            # this help
+     --rules           # prints rules
+
+     available commands:
+     add player <player_name>
+     move player
+     move player <roll1>, <roll2>],
+  rules => q[all the rules about special positions...],
+);
+
+
+  my $opts_parsed = {};
+
+  my @opt_keys = keys %OPTIONS;
+
+  GetOptions($opts_parsed, @opt_keys);
+
+  my @options =  keys %{$opts_parsed};
+
+  for my $opt (keys %{$opts_parsed}) { #user is learning
+    print "$opt: $opts_parsed->{$opt}\n";
+    if(defined $opts_parsed->{$opt}) {
+      print_option($OPTIONS{$opt});
+    }
+  }
+
+  reset_game(); #typically that would be called "init", but I use it also for cleanup after the end
+  main();
+
 
 sub main {
-  my $opts = {};
-  GetOptions($opts, qw(help));
 
-  my $items = \@ARGV;
-
-  if($opts->{help}) {
-    print <<"EOT" or croak qq[Error printing: $ERRNO];
-$PROGRAM_NAME v$VERSION
-
- The Game of the Goose.
-
- --help            # this help
-
- available command s at the prompt:
- add player <player_name>
- move player
- move player <roll1>, <roll2>
-
-EOT
-    exit;
-  }
   while (1) {
     print 'enter command: ';
     my $user_input_string = <STDIN>;
     my $user_input = [];
-    @{$user_input} = split /(\s|,)+/, $user_input_string;
-    print @{$user_input};
+    @{$user_input} = split /(?:\s|,)+/, $user_input_string;
+    # foreach my $z  (@{$user_input}) {
+    #   print "INPUT=$z\n";
+    # }
 
-    my $args = ParseArgs($user_input);
+    my $args = parse_args($user_input);
+
     if ($args->{error}) {
       print $args->{error};
-    } else {
-      print $args->{action};
-
+      print_option($OPTIONS{help});
     }
+    #print Dumper($args);
+
+
   }
+  return;
 }
 
+sub print_option { #just a placeholder in case if fancier formatting will be introduced
+  my $option = shift;
+  print qq[$option\n];
+  return;
+}
 
-sub ParseArgs {
-  my $items = shift;
+sub parse_args {
+  my ($items) = @_;
   my $args = {};
-  if ($items->[0] eq q[add] && $items->[1] eq q[player]) {
+
+  if (($items->[0] eq q[add]) && ($items->[1] eq q[player])) {
     $args->{action} = $items->[0];
     $args->{player} = $items->[2];
   } elsif ($items->[0] eq q[move]) {
     $args->{action} = $items->[0];
     $args->{player} = $items->[1];
     my ($lower, $upper) = (1, 6);
-print "action: $args->{action}, player: $args->{player}\n";
     if (! defined $items->[2]) {
-
-    } else {
-      for my $num ([$items->[2], $items->[3]]) {
-        print "NUM: $num\n";
-          my $is_between = (sort {$a <=> $b} $lower, $upper, $num)[1] == $num;
-
-          printf "$num is%s between $lower and $upper\n", $is_between ? "" : " not";
+      #roll here
+    } elsif (defined $items->[3]) {
+      my @rolls = @{$items}[2..3];
+      while (my ($index, $roll) = each @rolls) {
+        my $is_between = (sort {$a <=> $b} $lower, $upper, $roll)[1] == $roll;
+        if (!$is_between) {
+          $args->{error} = qq[move command should end with two integers from 1 to 6 each separated by white space or a comma\n];
+          last;
+        } else {
+          $args->{ 'roll_' . ++$index } = $roll;
+        }
       }
+
+      # foreach my $roll (@{$items}[2..3]) {
+      #   my $is_between = (sort {$a <=> $b} $lower, $upper, $roll)[1] == $roll;
+      #   #printf "$roll is%s between $lower and $upper\n", $is_between ? "" : " not";
+      #   if (!$is_between) {
+      #     $args->{error} = qq[move command should end with two integers from 1 to 6 each separated by white space or a comma\n];
+      #     last;
+      #   } else {
+      #     $args->{"roll$roll"} = $roll;
+      #   }
+      # }
+    } else {
+      $args->{error} = qq[move command should end with two integers from 1 to 6 each separated by white space or a comma\n];
     }
   } elsif ($items->[0] eq q[exit] || $items->[0] eq q[quit]) {
+    reset_game();
     exit;
   } else {
-    $args->{error} = qq[command should start with either "add" or "move"\n];
+    $args->{error} = qq[command should start with either "add player" or "move", follwed by player's name\n];
   }
   return $args;
 }
+
+sub signal_handler {
+  reset_game();
+  exit;
+}
+
+sub reset_game {
+  my $dir = q[database];
+  if (!-d $dir) {
+    eval {
+      make_path($dir);
+    } or do {
+      croak qq[Could not create directory $dir: $EVAL_ERROR];
+    }
+  }
+  return;
+}
+
+
+
+
+
+=head1 NAME
+
+The Game of the Goose
+
+=head1 VERSION
+
+$LastChangedRevision$
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SUBROUTINES/METHODS
+
+=head2 parse_args - parse user input command
+
+=head2 reset_game - remove users that were created during the game; makes sure database dir exist
+
+=head2 print_option - prints help option specified by the user from the command line
+
+=head2 signal_handler - signla handled, Ctrl+C , etc
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+$Author: Igor Pozdnyakov,,,$
+
+=head1 LICENSE AND COPYRIGHT
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ lib/player.pm
@@ -0,0 +1,105 @@
+# -*- mode: cperl; tab-width: 8; indent-tabs-mode: nil; basic-offset: 2 -*-
+# vim:ts=8:sw=2:et:sta:sts=2
+#########
+# Author: rmp
+#
+package player;
+use strict;
+use warnings;
+use CGI::Carp;
+use Readonly;
+use English qw(-no_match_vars);
+
+our $VERSION = q[0.0.1];
+
+our %rules = {};
+
+
+sub init {
+  my $self     = shift;
+
+
+  return;
+}
+
+sub create {
+  my $self     = shift;
+
+  return;
+}
+
+sub move {
+  my $self     = shift;
+  my $roll1 = shift;
+  my $roll2 = shift;
+
+  return;
+}
+
+sub name {
+  my $self = shift;
+  my $name = shift;
+
+  if (defined $name && $name) {
+    $self->{name} = $name;
+  }
+  return $self->{name};
+
+}
+
+sub position {
+  my $self     = shift;
+  my $position = shift;
+
+  if (defined $position && $position) {
+    $self->{position} = $position;
+  }
+  return;
+}
+
+=head1 NAME
+
+The Game of the Goose
+
+=head1 VERSION
+
+$LastChangedRevision$
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SUBROUTINES/METHODS
+
+=head2 init - instantiate player object
+
+=head2 create
+
+=head2 move
+
+=head2 position - returns current position of the user
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+=head1 AUTHOR
+
+$Author: Igor Pozdnyakov,,,$
+
+=head1 LICENSE AND COPYRIGHT
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=cut
